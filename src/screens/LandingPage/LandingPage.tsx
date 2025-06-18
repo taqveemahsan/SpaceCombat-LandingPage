a9d358f548d88f9f6584e849a7265279fc5c60ac
Taqveem Ahsan <57741221+taqveemahsan@users.noreply.github.com>
2025-06-18 18:22:30 +0500

    Remove root background color

--- src/screens/LandingPage/LandingPage.tsx
+++ src/screens/LandingPage/LandingPage.tsx
@@ -58,7 +58,7 @@ const gameScreenshots = [
 
 export const LandingPage = (): JSX.Element => {
   return (
-    <div className="relative bg-black flex flex-col items-center w-full overflow-hidden min-h-screen">
+    <div className="relative flex flex-col items-center w-full overflow-hidden min-h-screen">
       {/* Fixed hero background */}
       <div className="fixed inset-0 -z-10">
         <img
